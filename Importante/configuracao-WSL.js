98c3c95c65966e715ab10dff5f250af027d470fb
Fabiano <fasnawebdev@gmail.com>
2024-02-18 14:05:20 -0300

    Como instalar WSL

--- Importante/configuracao-WSL.js
+++ Importante/configuracao-WSL.js
@@ -2,6 +2,8 @@
      Boas Orientações
        Utilize um sistema baseado no sistema operacional (unix). Como Mec ou Linux.
        Caso você utilize o Windows como sistema operacional, utilise o (wsl).
+
+       
        WSL -> É uma tecnologia desenvolvida pela Microsoft que permite executar um ambiente 
               Linux dentro do sistema operacional Windows.Caso
     
