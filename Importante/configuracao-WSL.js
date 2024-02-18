4c9b56360fcdd6d2972b92013a713d6b09086d27
Fabiano <fasnawebdev@gmail.com>
2024-02-18 14:50:37 -0300

    Como instalar o WSL

--- Importante/configuracao-WSL.js
+++ Importante/configuracao-WSL.js
@@ -1,10 +1,23 @@
-     
-     Boas Orientações
-       Utilize um sistema baseado no sistema operacional (unix). Como Mec ou Linux.
-       Caso você utilize o Windows como sistema operacional, utilise o (wsl).
+  /*
 
-       
-       WSL -> É uma tecnologia desenvolvida pela Microsoft que permite executar um ambiente 
-              Linux dentro do sistema operacional Windows.Caso
+    LEGAL, MAS PRECISO APRENDER UNIX?
+    Aqui no curso nós recomendamos que você utilize um sistema operacional Unix Based, os mais conhecidos são:
     
-     
+    MacOS
+    Linux
+
+    Nossa, não uso esses sistemas operacionais hoje! Como vou fazer o curso? 😓
+    Não se preocupe! A Microsoft desenvolveu uma tecnologia chamada WSL (Windows Subsystem for Linux) que permite 
+    executar um ambiente Linux dentro do sistema operacional Windows.
+    O WSL oferece uma integração profunda entre o Windows e o Linux, permitindo que os desenvolvedores usem ferra-
+    mentas e aplicativos Linux diretamente em seus sistemas Windows.
+    
+    Com o WSL, é possível executar aplicativos e scripts Linux, acessar o sistema de arquivos Linux e até mesmo usar
+    um terminal Linux diretamente no Windows. Isso torna mais fácil para os desenvolvedores trabalhar em ambientes mistos,
+    nos quais aplicativos e serviços Linux são essenciais para o desenvolvimento, mesmo em sistemas baseados no Windows.
+    
+    No próximo conteúdo vamos te ensinar a instalar o WSL, o que permitirá que você, que usa Windows, continue com o sis-
+    tema operacional Windows, mas desfrute das vantagens de um sistema Linux.
+    
+  */
+    
