2f9c0356786032a1800766072e6737ecedcd036f
Fabiano <fasnawebdev@gmail.com>
2024-02-21 21:08:52 -0300

    O que é um terminal

---
+++ Importante/01_sobre-terminal.js
@@ -0,0 +1,27 @@
+         
+       O que é um Terminal ?
+
+       Terminal é uma interface  de linha de comando  que permite  aos usuários interagirem  com
+       um computador ou sistema operacional por meio de comandos de texto, facilitando tarefas Como
+       navegação em arquivos, administração de sistemas e programação, sem necessidade de uma inter-
+       face gráfica de usuário. 
+
+       Além disso na maioria das vezes, principalmente devido a questões de custo, performance e 
+       facilidade de interação no terminal, os servidores com sistemas operacionais baseados em Unix 
+       são as opções mais indicadas para você hospedar seus projetos web. Como servidores não possuem
+       interface alguma além do terminal, nesse cenário, conhecer seus comandos passa a ser essencial.
+
+       Por que fazemos uso da linha de comando se podemos resolver demandas em poucos cliques através 
+       da interface gráfica? De forma resumida, a linha de comando é simples, estável, rápida, eficiente
+       e exige baixíssimo poder de hardware.
+
+       Pode soar estranho dizer que ela é simples, não é? Ela pode parecer complicada porque ainda não faz
+       parte do seu dia a dia. Entretanto, a partir de agora, você vai conhecer detalhadamente as vantagens
+       e praticidades dessa ferramenta, o que o ajudará a ser uma pessoa desenvolvedora com maior produtivi-
+       dade e eficiência.
+
+       Observação 🔎
+       O uso da linha de comando não impede você de utilizar a interface gráfica. Com tempo e prática, você 
+       encontrará a forma de trabalho que proporciona a melhor experiência para você.
+
+   
