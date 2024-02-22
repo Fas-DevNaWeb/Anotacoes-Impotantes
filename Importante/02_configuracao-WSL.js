cd585dd3cd35cc942554cdbc67739e454108536c
Fabiano <fasnawebdev@gmail.com>
2024-02-21 21:09:42 -0300

    Configuração do WSL

---
+++ Importante/02_configuracao-WSL.js
@@ -0,0 +1,20 @@
+  
+
+    WSL - Use um Sistema baseado em Unix.
+
+    A Microsoft desenvolveu uma tecnologia chamada WSL (Windows Subsystem for Linux) que permite 
+    executar um ambiente Linux dentro do sistema operacional Windows.
+    O WSL oferece uma integração profunda entre o Windows e o Linux, permitindo que os desenvolvedores usem ferra-
+    mentas e aplicativos Linux diretamente em seus sistemas Windows.
+    
+    Com o WSL, é possível executar aplicativos e scripts Linux, acessar o sistema de arquivos Linux e até mesmo usar
+    um terminal Linux diretamente no Windows. Isso torna mais fácil para os desenvolvedores trabalhar em ambientes mistos,
+    nos quais aplicativos e serviços Linux são essenciais para o desenvolvimento, mesmo em sistemas baseados no Windows.
+    
+    Instalação no Ambiente Windows - WSL 
+    Abra o powershell do windows e digite o comando: wsl --install
+    Ele vai pedir uma autorização para instalar, e preciso aceitar para instalar.
+    Apos a instalação reinicie o computador.
+    Abra o terminal novamente. Ele vai pedir para criar um usuário no Ubuntu e inserir sua senha.
+
+    A partir dai você consegue executar comando do linux no windows.
