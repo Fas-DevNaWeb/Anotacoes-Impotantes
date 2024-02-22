fec2e4029d228907e3d8412021f221259f57bc05
Fabiano <fasnawebdev@gmail.com>
2024-02-21 22:37:57 -0300

    Intalação GIT

---
+++ Importante/04_instalacao-doGit.js
@@ -0,0 +1,21 @@
+Instalação em Linux / WSL
+Para instalar o Git, abra seu terminal e digite o comando:
+
+Copiar
+sudo apt install git
+
+
+Atenção: Se você iniciar um diretório com (git init),verifique se sua branch está iniciada 
+         como main, caso não esteja, esteja iniciada como master, digite o comando:
+         (git config --global init.defaultBranch main)
+         Digite o comando (ls -a)
+         e depois exclua a pasta .git, usando o comando (rm -rf .git)
+
+         Após isso inicie novavamente o diretório, o mesmo ira voltar com a branch
+         iniciada como main.Após
+
+         OBS: Os parenteses etre os codigos mostrados acima, não faz parte do código é 
+         simplesmente para separar o código do resto do Texto.
+
+         OBS: Master caiu em desuso, não se usa mais.Utiliza-se main como branch principal.
+
