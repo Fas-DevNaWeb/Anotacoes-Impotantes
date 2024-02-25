6f7cc09914a75495397069a5acaefd0ca749c007
Fabiano <fasnawebdev@gmail.com>
2024-02-24 21:13:11 -0300

    Aprendendo comandos do git

---
+++ Importante/06_comandos-do-GIT.js
@@ -0,0 +1,15 @@
+Coamndos Do Geolocation
+
+  1° - git clone + (url do github) -> baixa o repositório para seu computador.
+  2° - git commit -m "Sobre o que se trata o commit" -> Cria uma nova versão do seu codigo
+  3° - git branch -> Branch é o nome dado a uma versão do seu código, quando você cria um 
+       repósitório ele ja vem com uma branch iniciada como mai. É nessa branch que fica o inicio 
+       do seu Projeto. A partir dela você pode fazer uma cópia e ir incrementando seu projeto, até 
+       que tudo estege pronto e você a inclua na sua branch principal main.
+       O -comando git branch mostra as branch que existe no seu repositório.
+
+  4° - git checkout -b + (Nome da branch que você que criar) -> Cria uma nova branch com nome que 
+       você criou. É uma cópia da branch main(principal).A partir dai você trabalha na nova branch
+       até quando achar necessário.Quando tudo estiver concluido você uni essa branch a branch principal.
+  
+  5° - git checkout 
