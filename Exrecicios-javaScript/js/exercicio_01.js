fd0ae749241090bc22a5fd96b90e702c9c9f968d
Fabiano <fasnawebdev@gmail.com>
2024-03-23 16:56:53 -0300

    Exercicios 1 2 dois completados

---
+++ Exrecicios-javaScript/js/exercicio_01.js
@@ -0,0 +1,6 @@
+const myName = 'Fabiano Alves Santos';
+const birthCity = 'Rio de Janeiro';
+const birthYear = 1981;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ Exrecicios-javaScript/js/exercicio_02.js
@@ -0,0 +1,10 @@
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = base * 2 + height * 2;
+
+console.log('Sobre o Retangulo');
+console.log('Base: ' + base);
+console.log('Altura: ' + height);
+console.log('Área: ' + area);
+console.log('Perimetro: ' + perimeter);
