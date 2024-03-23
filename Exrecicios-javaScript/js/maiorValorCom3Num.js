fed69bc0671722dd50d34c4cf2345035d4efc0be
Fabiano <fasnawebdev@gmail.com>
2024-03-23 20:57:21 -0300

    Organizando os exercicios

---
+++ Exrecicios-javaScript/js/maiorValorCom3Num.js
@@ -0,0 +1,13 @@
+let n1 = 122;
+let n2 = 769;
+let n3 = 900;
+
+if (n1 > n2 && n1 > n3) {
+    console.log('O primeiro numero é o maior: ' + n1);
+}else if(n2 > n1 && n1 > n3){
+    console.log('O segundo numero é o maior: ' + n2);
+}else{
+    console.log('O terceiro numero é o maior: ' + n3);
+
+}
+
