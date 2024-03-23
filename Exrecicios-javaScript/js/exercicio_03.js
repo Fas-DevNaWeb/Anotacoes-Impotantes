a91d77fc6fe80651fe7e4f3b8992b90d9805749a
Fabiano <fasnawebdev@gmail.com>
2024-03-23 17:20:18 -0300

    Exercicio 03 concluido

---
+++ Exrecicios-javaScript/js/exercicio_03.js
@@ -0,0 +1,9 @@
+let grade = 66;
+
+if (grade > 70) {
+  console.log('Aluno Aprovado!!!');
+} else if ((grade > 60) & (grade < 70)) {
+  console.log('Aluno em Lista de espera.');
+} else {
+  console.log('Aluno Reprovado.');
+}
