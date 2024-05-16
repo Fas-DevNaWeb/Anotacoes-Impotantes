7c03c57046e66db7addc38b7e3daaeacd42d6cf6
Fabiano <fasnawebdev@gmail.com>
2024-05-15 23:24:06 -0300

    Bora pra cima

--- Exrecicios-javaScript/js/mapParte2.js
+++ Exrecicios-javaScript/js/mapParte2.js
@@ -1,8 +1,56 @@
 // ADICIONAR UMA PROPRIEDADE PARA CADA ELEMENTO DO ARRAY
-const data = require('./data');
+const data = require("./data");
+//console.log(data);
 
-const addProperty = (obj,propName,proValue) => {
-   return data.m
-}
+// Primeira forma .....
+const addProperty = (obj, propName, proValue) => {
+  return arrayNewProp = obj.map((cards) => {
+    const id = cards.id;
+    const name = cards.name;
 
-console.log(addProperty(data.cards,'game','yu-Gi-Oh!'));
+    return {
+      id: id,
+      name: name,
+      [propName]: proValue,
+    }
+  });
+};
+
+console.log(addProperty(data.cards, 'game', 'yu-Gi-Oh!'));
+
+// Segunda forma contraida ....
+const addProperty2 = (obj, propName, proValue) => {
+  return arrayNewProp = obj.map((cards) => {
+    const id = cards.id;
+    const name = cards.name;
+
+    return {
+      id,
+      name,
+      [propName]: proValue,
+    }
+  });
+};
+
+console.log(addProperty2(data.cards, 'game', 'yu-Gi-Oh!'));
+
+
+// Forma contraida com spreed .....
+const addProperty3 = (obj, propName, proValue) => {
+  return arrayNewProp = obj.map((cards) => {
+    
+    return {
+       ...cards,
+      [propName]: proValue,
+    }
+  });
+};
+
+console.log(addProperty3(data.cards, 'game', 'yu-Gi-Oh!'));
+
+
+// Forma ainda mais contraida em uma linha .....
+const addProperty4 = (obj, propName, proValue) => obj.map((cards) =>  ({...cards,[propName]:proValue}));
+
+  
+console.log(addProperty4(data.cards, 'game', 'yu-Gi-Oh!'));
