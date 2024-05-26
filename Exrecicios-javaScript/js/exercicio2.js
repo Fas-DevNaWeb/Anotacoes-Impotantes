7cfead770bb0f12860a978edfac9caa681eca570
Fabiano <fasnaweb2020@gmail.com>
2024-05-26 19:14:35 -0300

    Bora pra cima mais um exercicio completo

--- Exrecicios-javaScript/js/exercicio2.js
+++ Exrecicios-javaScript/js/exercicio2.js
@@ -1,2 +1,40 @@
-const countries = require('./countries');
-console.log(countries);
+const countries = require("./countries");
+//console.log(countries);
+/*
+Exercício 24
+Para cada país, crie um objeto apenas com as informação do nome, 
+código da moeda e nome da moeda. Essas informações devem ser
+armazenadas em um novo array. Faça esse exercício usando o 
+método forEach, não vale map. 😜
+
+Exemplo de como devem ser os objetos:
+
+  Copiar
+   {
+     name: 'United States',
+     currencyCode: 'USD',
+     currencyName: 'United States Dollar',
+   }
+*/
+
+const createNewArray = (obj) => {
+  const newArray = [];
+  let name;
+  let code;
+  let nameCrr;
+
+  obj.forEach((element) => {
+    name = element.name;
+    code = element.currencies[0].code;
+    nameCrr = element.currencies[0].name;
+
+    newArray.push( {
+      nameRegion: name,
+      currencyCode:code,
+      currencyName:nameCrr,
+    });
+  });
+  console.log(newArray);
+};
+
+createNewArray(countries);
