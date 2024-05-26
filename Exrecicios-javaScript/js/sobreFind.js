9a5e6d15ebc514cb4cc572b15d89d64ab55a5d7c
Fabiano <fasnaweb2020@gmail.com>
2024-05-25 23:55:58 -0300

    Bora pra cima denovo sobre o metodo find() parte 1

--- Exrecicios-javaScript/js/sobreFind.js
+++ Exrecicios-javaScript/js/sobreFind.js
@@ -2,3 +2,14 @@
  *  Caracteristica -> O método find(); Retorna um Elemento ao invés de um array, 
  *  e sempre o primeiro elemento encontrado conforme a condição passada.
  */
+
+const data = require('./data');
+// ENCONTRE UMA CARTA DE ACORDO COM SEU NOME
+
+const findCard = (data,cardName) => {
+
+    const cardFound = data.find((card) => card.name === cardName);
+    return cardFound;
+};
+
+console.log(findCard(data.cards, 'Dark Magician'));
