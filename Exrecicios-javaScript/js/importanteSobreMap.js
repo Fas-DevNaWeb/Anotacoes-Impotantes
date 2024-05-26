fed25526d1b8916c5e8aaa33555de5255cedce71
Fabiano <fasnaweb2020@gmail.com>
2024-05-25 22:17:54 -0300

    Bora pra cima denovo

--- Exrecicios-javaScript/js/importanteSobreMap.js
+++ Exrecicios-javaScript/js/importanteSobreMap.js
@@ -1,12 +1,12 @@
 /**
  *  O método map(); ele retorna um array de acordo com a sua finalidade, pode ser um array de objetos 
  *  ou de números, ou string em fim o fato é que ele retorna um ARRAY.
- */
 
-/**
- * O método filter(); ele retorna um array com o  objeto inteiro apenas retorna conforme a regra passada.
- */
 
-/**
- *  O método sort(); ordena de ordem crescente ou decrescente de acordo com sua aplicação.
+* O método filter(); ele retorna um array com o  objeto inteiro apenas retorna conforme a regra passada.
+ 
+
+*  O método sort(); ordena de ordem crescente ou decrescente de acordo com sua aplicação.
+
+*  O método find(); Retorna o primeiro elemento encontrado conforme a regra passada.Não retorna um array.
  */
